4004d6c8a0d82d561635267b573e0017ad2b91d0
jsostaric <bugificiran@gmail.com>
2022-09-22 19:56:01 +0200

    update docker-compose and clean index page

--- html/index.php
+++ html/index.php
@@ -1,16 +1,1 @@
-<?= 'Hello' ?>
-<h1>world</h1>
-
-<?php
-    $a = 2;
-    $b = 4;
-    echo 'variable $a is:' . $a . '<br>';
-    echo 'variable $b is:' . $b . '<br>';
-    $c = $a++ + ++$b;
-
-    echo 'variable $a is:' . $a . '<br>';
-    echo 'variable $b is:' . $b . '<br>';
-    echo 'variable $c is:' . $c . '<br>';
-    echo 'variable $a is:' . $a . '<br>';
-
-?>
+<?= 'Hello Docker' ?>
